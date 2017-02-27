de64b4e86ce7abcfbcc78b4d4794184aa95f2a62
rettekram <mark.etter@gmail.com>
2017-02-26 17:01:44 -0800

    Revise theme branding

--- sites/all/themes/custom/andersenreps/templates/page.tpl.php
+++ sites/all/themes/custom/andersenreps/templates/page.tpl.php
@@ -1,54 +1,58 @@
 <div id="page">
  
-  <div id="main">
-		
-    <div id="content" class="column" role="main">
-    	<div id="logo-wrapper">
-    		<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-    	</div>
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
+	<div id="main">
+
+		<div id="content" class="column" role="main">
+			<div id="logo-wrapper">
+				<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+			</div>
+			<?php print render($page['highlighted']); ?>
+			<?php print $breadcrumb; ?>
+			<a id="main-content"></a>
+			<?php print render($title_prefix); ?>
+			<?php if ($title): ?>
+				<h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+  			<?php endif; ?>
+			<?php print render($title_suffix); ?>
+			<?php print $messages; ?>
+			<?php print render($tabs); ?>
+			<?php print render($page['help']); ?>
+			<?php if ($action_links): ?>
+				<ul class="action-links"><?php print render($action_links); ?></ul>
+			<?php endif; ?>
+			<?php print render($page['content']); ?>
+			<?php print $feed_icons; ?>
+		</div>
 
 		<div id="navigation">
-	    <?php print render($page['navigation']); ?>
-	  </div>
-	  
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
-
-  </div>
-
-  <?php print render($page['footer']); ?>
+			<?php print render($page['navigation']); ?>
+		</div>
   
-  <div id="copyright">
-  	<p>&copy; 2004-<?php print date("Y"); ?> Andersen and Associates, Inc. | <a href="http://www.VastArray.com" target="_blank" rel="nofollow" >Designed by VastArray</a></p>
-  </div>
+		<?php
+		  // Render the sidebars to see if there's anything in them.
+		  $sidebar_first  = render($page['sidebar_first']);
+		  $sidebar_second = render($page['sidebar_second']);
+		?>
+
+		<?php if ($sidebar_first || $sidebar_second): ?>
+		  <aside class="sidebars">
+		<?php print $sidebar_first; ?>
+		<?php print $sidebar_second; ?>
+		  </aside>
+		<?php endif; ?>
+
+	</div>
+
+	<?php print render($page['footer']); ?>
   
+	<div id="copyright">
+		<p>&copy; 2004-<?php print date("Y"); ?> Andersen Arts Group</p>
+	</div>
+  
+</div>
+
+<div id="footer">
+	&nbsp;
 </div>
 
 <?php print render($page['bottom']); ?>
